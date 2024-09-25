13fac9197ce3962ce06bf9c8069cee5bf9e3aaa8
mattchristenson <matt@rcc.team>
2024-09-25 14:09:27 -0500

    I wrote my first shopping list app!

---
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+import java.util.SortedMap;
+
+public class ShoppingList {
+
+
+    public static void main(String[] args)
+    {
+        System.out.println("Shopping list:");
+        System.out.println("-------------------");
+        System.out.println("Apples");
+        System.out.println("Olives");
+    }
+}
